b09083288a2ce4fd19101077932088e7834603b9
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-02-02 18:01:13 +0100

    chore: Initial commit
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Story of their life
+
+You have two decks of cards. Deck one contains job titles, deck two contains termination reasons. Use them as a prompt what happened!
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	const deckOne: string[] = [];
+	const deckTwo: string[] = [];
+</script>
+
+<div class="body">
+	<section class="board">
+		<button on:click={() => {}} class="deck job"></button>
+		<button on:click={() => {}} class="deck termination"></button>
+		<div class="card job"></div>
+		<div class="card termination"></div>
+	</section>
+</div>
+
+<style>
+	.body {
+		display: grid;
+		place-items: center;
+	}
+
+	.deck,
+	.card {
+		border: 2px solid black;
+		border-radius: 12px;
+		height: 300px;
+		width: 200px;
+		position: absolute;
+	}
+
+	.card {
+		border: 2px dashed black;
+	}
+
+	.deck {
+		top: 0;
+	}
+
+	.card {
+		bottom: 0;
+	}
+
+	.job {
+		left: 0;
+	}
+	.termination {
+		right: 0;
+	}
+
+	.deck.job {
+		background: blue;
+	}
+	.deck.termination {
+		background: green;
+	}
+
+	.board {
+		position: relative;
+		border: 1px solid red;
+		height: 700px;
+		width: 500px;
+	}
+</style>
